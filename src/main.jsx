0f95c36b4ae1656a9054f2dc630e989e7e6da3cc
Renato Quiroz <renatoquiroz5500@gmail.com>
2025-04-09 15:23:01 -0400

    Instalación de liberaría CoreUI, Bootstrap y configuración de npm start

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
